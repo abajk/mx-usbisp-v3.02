cd028e7df70b988ae431c30caf0cb887c2771043
Aleksander Jan Bajkowski <olek2@wp.pl>
2019-07-02 22:21:47 +0200

    initial commit

---
+++ mx-usbisp-v3.02/Release/makedep.mk
@@ -0,0 +1,18 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+clock.c
+
+isp.c
+
+main.c
+
+oddebug.c
+
+tpi.S
+
+usbdrv.c
+
+usbdrvasm.S
+
